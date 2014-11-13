7aaf94a60c3bbfb182bfe12b33cae8c7e1de1c5d
inuyashazaq <inuyashazaq@gmail.com>
2014-11-13 17:12:36 +0800

    Revert "Revert "+pay""
    This reverts commit 76c64640a5e9c22437ec825e310a184a2cbd1226.

--- notify.php
+++ notify.php
@@ -3,8 +3,13 @@ $input_data = json_decode(file_get_contents("php://input"), true);
 if($input_data['object'] == 'charge')
 {
     //TODO update database
-    
-    echo 'success';
+     $filename = "not.txt";
+     $data = file_get_contents("php://input");
+     file_put_contents($filename, $data, FILE_APPEND);
+     $current_date = date("Ymd");
+     $current_time = date("H:i"); //add s if need seconds
+     file_put_contents($filename, "\n------------------'$current_date'-'$current_time'--------------------\n", FILE_APPEND);
+     echo 'success';
 }
 else if($input_data['object'] == 'refund')
 {
