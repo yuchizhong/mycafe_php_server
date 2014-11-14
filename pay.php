40991eca14d808e1aaf905189f65325dd4882eb1
inuyashazaq <inuyashazaq@gmail.com>
2014-11-14 18:11:20 +0800

    ping++ extra empty array

--- pay.php
+++ pay.php
@@ -20,9 +20,9 @@ $uname = $input_data['username'];
 $key = 'sk_test_rrzLCSebzbT8SKiH4GX9SWH8';
 $appid = 'app_5qjfH0GKyPy5y5ar';
 
-/*
 //$extra 在渠道为 upmp_wap 和 alipay_wap 时，需要填入相应的参数，具体见技术指南。其他渠道时可以传空值也可以不传。
 $extra = array();
+/*
 switch ($channel) {
     case 'alipay_wap':
         $extra = array(
