45a9a2763ff28f5b0d282e9551e65c085343c33c
yuchizhong <yuchizhong@163.com>
2015-01-06 23:31:47 +0800

    push bug fix

--- push.php
+++ push.php
@@ -24,7 +24,7 @@ $current_date = date("Ymd");
 $current_time = date("H:i"); //add s if need seconds
 mysql_query("INSERT INTO notifications VALUES ('$userID', '$current_date', '$current_time', '$msg', '0')");
 
-$result = mysql_query("SELECT push_token FROM UUID_user WHERE userID='$userID'");
+$result = mysql_query("SELECT push_token FROM UUID_user WHERE userID='$userID' AND platform=0 AND push_token IS NOT NULL AND push_token<>''");
 while ($row = mysql_fetch_array($result)) {
     // Provide the Device Identifier (Ensure that the Identifier does not have spaces in it).
     // Replace this token with the token of the iOS device that is to receive the notification.
